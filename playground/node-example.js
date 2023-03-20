e7b561d936d9495f8e52fd31aaa28a3c35d3e5f7
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-20 14:36:58 +1300

    Added files for variables and node-example

---
+++ playground/node-example.js
@@ -0,0 +1,2 @@
+console.log('I am from Node JS')//Go to the command line and CD to the directory and run node by entering noede
+// node-example.js to run in Node
---
+++ playground/variables.js
@@ -0,0 +1,8 @@
+var first = "Matt"; //variable declaration statement
+let age = 100;
+const cool = true;
+console.log(first, age, cool); //function call statement
+
+if (age > 10) {
+  console.log("You are old");
+}
