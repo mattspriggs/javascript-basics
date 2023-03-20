ab8b47f75c86cc9621c02f25062c10b12e93dee3
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-20 14:45:43 +1300

    More variables

--- playground/variables.js
+++ playground/variables.js
@@ -1,8 +1,12 @@
+"use strict";
+
 var first = "Matt"; //variable declaration statement
 let age = 100;
-const cool = true;
+const cool = true; //can only have the value of an array or object changed
 console.log(first, age, cool); //function call statement
 
 if (age > 10) {
   console.log("You are old");
 }
+
+// dog = "snickers"; //will not error if not in strict mode - bad practice, should always be declared
