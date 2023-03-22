870d841846c4ad076777dcaaa509cb25077b87fb
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-22 15:52:36 +1300

    Finished Eslint and Prettier tooling

--- playground/variables.js
+++ playground/variables.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 //function scoped
-var first = "Matt"; //variable declaration statement
+var first = 'Matt'; //variable declaration statement
 
 //block scoped
 let age = 100;
@@ -17,7 +17,7 @@ const cool = true; //can only have the value of an array or object changed
 console.log(first, age, cool); //function call statement
 
 if (age > 10) {
-  console.log("You are old");
+  console.log('You are old');
 }
 
 // dog = "snickers"; //will not error if not in strict mode - bad practice, should always be declared
