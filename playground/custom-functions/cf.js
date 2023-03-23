caf650bc0bbadfeb95d6ea63ed28391932f0373e
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-24 12:47:15 +1300

    Working on functions

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,13 @@
+console.log('it works!');
+//This is the function definition
+function calculateBill() {
+  //function block {}
+  //this is the function body
+  console.log('Running Calculate Bill!!');
+  const total = 100 * 1.13;
+  return total;
+}
+//Return will provide what the conclusion of the function is
+
+//Function call or run of the function
+calculateBill();
