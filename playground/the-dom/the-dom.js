905a044957196a63a75f3dc0f62b22873ed9afed
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-28 15:56:28 +1300

    Basics of the DOM selections

--- playground/the-dom/the-dom.js
+++ playground/the-dom/the-dom.js
@@ -1,2 +1,12 @@
+//Two main ways to get DOM elements: querySelector() and querySelectorAll()
+
 const p = document.querySelector('p');
+const divs = document.querySelectorAll('div');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img'); //searching inside the element
 console.log(p);
+console.log(divs); //returns a node list (not an array)
+console.log(imgs);
+console.log(item2);
+console.log(item2Image);
