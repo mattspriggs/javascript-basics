724ea817ad08145c8b81ac2701f13a1d7079650f
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-29 14:54:42 +1300

    Working with sectors

--- playground/the-dom/the-dom.js
+++ playground/the-dom/the-dom.js
@@ -5,8 +5,16 @@ const divs = document.querySelectorAll('div');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img'); //searching inside the element
-console.log(p);
-console.log(divs); //returns a node list (not an array)
-console.log(imgs);
-console.log(item2);
-console.log(item2Image);
+// console.log(p);
+// console.log(divs); //returns a node list (not an array)
+// console.log(imgs);
+// console.log(item2);
+// console.log(item2Image);
+
+const heading = document.querySelector('h2');
+// heading.textContent = 'Matt is cool'; //Setter
+// console.dir(heading.textContent); //getter for everything including style and other elements; will show something is
+// // hidden as well
+// console.dir(heading.innerText); //same as the above but only human-readable
+console.log(heading.textContent);
+console.log(heading.innerText);
