cff4aebe3a13897712b3a8434c87e89e5ea1cfb9
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-03 17:03:53 +1200

    Making a single re-render for a div, paragraph and image

--- playground/the-dom/creating.js
+++ playground/the-dom/creating.js
@@ -5,4 +5,15 @@ myParagraph.classList.add('special');
 
 const myImage = document.createElement('img');
 myImage.src = 'https://source.unsplash.com/random/300x300';
+myImage.alt = 'Nice photo';
 console.log(myImage);
+
+const myDiv = document.createElement('div');
+myDiv.classList.add('wrapper');
+console.log(myDiv);
+
+myDiv.appendChild(myParagraph); //This will require only one re-render of the page
+myDiv.appendChild(myImage);
+document.body.appendChild(myDiv); //each will cause a re-render of the page
+// myDiv.appendChild(myParagraph);
+// myDiv.appendChild(myImage);
