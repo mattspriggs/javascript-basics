f36aa483a841e0587e109a3ac09feb24d860ff5b
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-03 16:55:02 +1200

    Adding elements to the page using createElement

---
+++ playground/the-dom/creating.js
@@ -0,0 +1,8 @@
+const myParagraph = document.createElement('p');
+console.log(myParagraph);
+myParagraph.textContent = 'I am a P';
+myParagraph.classList.add('special');
+
+const myImage = document.createElement('img');
+myImage.src = 'https://source.unsplash.com/random/300x300';
+console.log(myImage);
