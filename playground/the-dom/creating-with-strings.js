67ed8ac0c17cdd3a8043357c4aa9cc6de009b205
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-05 11:01:27 +1200

    Using fragments to crete true HTML

--- playground/the-dom/creating-with-strings.js
+++ playground/the-dom/creating-with-strings.js
@@ -5,14 +5,24 @@ const src = `https://source.unsplash.com/random/300x${width}`;
 const desc = `Cute Pup`;
 
 const myHTML = `
-	<div className="wrapper">
+	<div class="wrapper">
 		<h2>${desc}</h2>
 		<img src="${src}" alt="${desc}">
 	</div>`;
+//TURN A STRING INTO A DOM ELEMENT
+const myFragment = document.createRange().createContextualFragment(myHTML); //turns string into true DOM elements
+
+console.log(myFragment);
+console.log(myFragment.querySelector('img'));
+document.body.appendChild(myFragment);
 // item.innerHTML = `
 // 	<div>
 // 		<h1>Hey, how are you?</h1>
 // 	</div>
 // `; //setter
-item.innerHTML = myHTML;
-console.log(item.innerHTML); //getter
+// item.innerHTML = myHTML;
+// console.log(item.innerHTML); //getter
+
+// const itemImage = document.querySelector('.wrapper img');
+//
+// itemImage.classList.add('round');
