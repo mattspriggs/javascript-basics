065609d810d701b4f89c07a68dadae37de641621
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-05 10:45:12 +1200

    Working with HTML from javaScript strings

--- playground/the-dom/creating-with-strings.js
+++ playground/the-dom/creating-with-strings.js
@@ -1,3 +1,18 @@
-const item = document.querySelector('item');
+const item = document.querySelector('.item');
 
-console.log(item.innerHTML);
+const width = 500;
+const src = `https://source.unsplash.com/random/300x${width}`;
+const desc = `Cute Pup`;
+
+const myHTML = `
+	<div className="wrapper">
+		<h2>${desc}</h2>
+		<img src="${src}" alt="${desc}">
+	</div>`;
+// item.innerHTML = `
+// 	<div>
+// 		<h1>Hey, how are you?</h1>
+// 	</div>
+// `; //setter
+item.innerHTML = myHTML;
+console.log(item.innerHTML); //getter
