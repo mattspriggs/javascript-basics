e739becba9238e452a858b85b320fca0060f81af
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-12 15:38:32 +1200

    Working with the event object a bit deeper

--- exercises/Events/events.js
+++ exercises/Events/events.js
@@ -57,13 +57,37 @@ const buyButtons = document.querySelectorAll('button.buy');
 // buyButtons.forEach(attachListener);
 
 function handleBuyButtonClick(event) {
+  console.log('You clicked a button');
   const button = event.target;
-  console.log(button.textContent);
-  console.log(parseFloat(event.target.dataset.price));
+  // console.log(button.textContent);
+  // console.log(parseFloat(event.target.dataset.price));
+  console.log(event.target);
+  console.log(event.currentTarget);
+  console.log(event.target === event.currentTarget); // They are the same unless it is nested in which case the
+  // target is the nested item but the currentTarget is the object whole that is clicked - better to use currentTarget
+  // Stop PROPAGATION - bubbling up
+  // event.stopPropagation();
 } //Adding data to the button and accessing the value and parse the info to a float to preserve decimals
 
 buyButtons.forEach(function (buyButton) {
   buyButton.addEventListener('click', handleBuyButtonClick);
 });
 
+window.addEventListener(
+  'click',
+  function (event) {
+    //   Capture phase happens on the way down you can stop using a 3rd parameter of and option
+    console.log('You clicked the window!');
+    console.log(event.target);
+    console.log(event.type);
+    console.log(event.bubbles); //Boolean about if it will propagate or not
+    // event.stopPropagation(); //Will get only the capture on the window
+  },
+  { capture: true }
+); //Overlaps and both will fire via propagation - the event bubbles up - strong tag, button, html
+// tag, body,
+// window, etc. as it moves up
+
 //THE EVENT OBJECT
+//The event object contains a lot of information about the event that has happened
+//The event target is of most use; you can get teh data from the target including the location and the text content
