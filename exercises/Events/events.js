caacbddd5f7cdd6d36ff64b8281ada89153954c4
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-12 13:13:46 +1200

    Working with event listeners

--- exercises/Events/events.js
+++ exercises/Events/events.js
@@ -1,1 +1,16 @@
-console.log('it works');
+const butts = document.querySelector('.butts');
+
+//butts.addEventListener('eventType', callBackFunction) - callback function is one that is called when an event
+// occurs - it is a function that is called when it is passed to a method like eventListeners
+
+function handleClick() {
+  console.log('it got clicked');
+}
+butts.addEventListener('click', handleClick);
+// butts.addEventListener('click', function () {
+//   console.log('it got clicked');
+// });
+//Three-step process:
+// butts - GO GET SOMETHING -.addEventListener  ('click' - LISTEN FOR SOMETHING - , function () {
+// 	console.log('it got clicked') - DO SOMETHING_;
+// });
