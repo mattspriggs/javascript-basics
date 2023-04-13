035786a7e182d421fe787ec81495f38fba405b2d
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-13 15:02:36 +1200

    Basic event and accessibility and keycodes complete

--- exercises/Events/forms.js
+++ exercises/Events/forms.js
@@ -35,16 +35,31 @@ signupForm.addEventListener('submit', function (event) {
 });
 
 //COMMON EVENTS
-//'keyup'
 function logEvent(event) {
   console.log(event.type);
   console.log(event.currentTarget.value);
 }
+//'keyup'
 signupForm.name.addEventListener('keyup', logEvent);
-
 //'keydown'
 signupForm.name.addEventListener('keydown', logEvent);
 //'focus'
 signupForm.name.addEventListener('focus', logEvent); //cursor is in and input
 //'blur'
 signupForm.name.addEventListener('blur', logEvent); //cursor moves out of the input
+
+//ACCESSIBILITY
+//Buttons are to be used for something to work within the application, links are to be used to change the page - do
+// not use them to replace each other
+
+const photo = document.querySelector('.photo');
+
+function handlePhotoClick(event) {
+  if (event.type === 'click' || event.key === 'Enter')
+    console.log('You clicked the photo');
+}
+photo.addEventListener('click', handlePhotoClick);
+photo.addEventListener('keyup', handlePhotoClick); //will capture the enter key on the photo
+//a photo cannot be focussed on with a keyboard only if it is just added an
+// event listener - add a role="button" in HTML to allow it to be selected with a tab key or better just to a
+// button,  but if you need to do something in HTML like use a div for a button it should be like the one on the HTML
