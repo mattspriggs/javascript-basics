f8d34992dcf45683e3e59f8220181e89c317fdd5
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-17 16:26:32 +1200

    Working on the modal selction information to populate the modal

--- exercises/outside-the-modal/click-outside.js
+++ exercises/outside-the-modal/click-outside.js
@@ -1,1 +1,13 @@
-console.log('it works!');
+const cardButtons = document.querySelectorAll('.card button');
+
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card'); //closest will look up the DOM instead of down like querySelectorAll
+  //Grab the img source
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  console.log(desc);
+}
+cardButtons.forEach((button) =>
+  button.addEventListener('click', handleCardButtonClick)
+);
