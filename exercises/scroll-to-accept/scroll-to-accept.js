f77993d50e17aa11ac50b5cbb489fbb71792fe01
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-18 14:11:06 +1200

    Adding the intersection observer

--- exercises/scroll-to-accept/scroll-to-accept.js
+++ exercises/scroll-to-accept/scroll-to-accept.js
@@ -1,1 +1,21 @@
-console.log('it works');
+function scrollToAccept() {
+  const terms = document.querySelector('.terms-and-conditions');
+  const watch = document.querySelector('.watch');
+
+  function obCallBack(payload) {
+    console.log(payload);
+  }
+  const ob = new IntersectionObserver(obCallBack);
+  ob.observe(watch);
+
+  if (!terms) {
+    return; //quits the function without item on page
+  }
+
+  // terms.addEventListener('scroll', function (e) {
+  // console.log(e.currentTarget.scrollTop); No longer used wit the introduction of intersection observer
+  //   console.log(e.currentTarget.scrollHeight); No longer used wit the introduction of intersection observer
+  // });
+}
+
+scrollToAccept();
