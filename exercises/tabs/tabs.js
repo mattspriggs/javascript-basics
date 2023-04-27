b622d271d96288e521cfa978e2ce8e6f51ae0be8
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-27 15:53:57 +1200

    Working on the changing of the properties

--- exercises/tabs/tabs.js
+++ exercises/tabs/tabs.js
@@ -4,9 +4,15 @@ const tanPanels = tabs.querySelectorAll('[role="tabpanel"]');
 
 function handleTabClick(event) {
   //When the tab is clicked hide all others
-  console.log(tanPanels);
+  tanPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
   //Mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
   //Mark the clicked tab as selected
+
   //Find the tab panel and show it
 }
 tabButtons.forEach((button) =>
