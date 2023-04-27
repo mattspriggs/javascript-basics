4b2fb8a3c8b488b44638c48c8daae320c7f64457
Matt Spriggs <mspriggs1@cnm.edu>
2023-04-27 16:50:20 +1200

    tabs complete

--- exercises/tabs/tabs.js
+++ exercises/tabs/tabs.js
@@ -1,10 +1,10 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tanPanels = tabs.querySelectorAll('[role="tabpanel"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
   //When the tab is clicked hide all others
-  tanPanels.forEach((panel) => {
+  tabPanels.forEach((panel) => {
     panel.hidden = true;
   });
   //Mark all tabs as unselected
@@ -15,8 +15,18 @@ function handleTabClick(event) {
   event.currentTarget.setAttribute('aria-selected', true);
   //Find the tab panel and show it
   const { id } = event.currentTarget;
+
+  /* METHOD 1
   const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
   console.log(tabPanel);
+  tabPanel.hidden = false;
+   */
+
+  //   METHOD 2- FIND IN THE ARRAY OF TABS
+  console.log(tabPanels);
+  const tabPanel = tabPanels.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id
+  );
   tabPanel.hidden = false;
 }
 tabButtons.forEach((button) =>
