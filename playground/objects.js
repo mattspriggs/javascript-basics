6e6f87b92b040ae902eda927a0e6a8bb165bf5ad
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-03 10:09:05 +1200

    Setup for object work

---
+++ playground/objects.js
@@ -0,0 +1,11 @@
+//Objects allow you to store related properties (key) and values  - order may not be maintained - can contain any type
+const age = 100;
+
+const person = {
+  name: 'Matt',
+  age, //age: age,
+  'cool-dude': true,
+  'really cool': false,
+  777: true,
+};
+console.log(person);
