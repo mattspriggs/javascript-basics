4180c4867e30277bdea1807997b2a33d7e55f229
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-03 09:43:58 +1200

    Intervals and timeout complete

--- playground/intervals.js
+++ playground/intervals.js
@@ -7,7 +7,7 @@ setTimeout(function () {
 }, 500); //globally scoped method
 
 console.log('Starting');
-setTimeout(buzzer, 500); //demonstrates the asynchronous nature of JavaScript as a callback at alter time
+// setTimeout(buzzer, 500); //demonstrates the asynchronous nature of JavaScript as a callback at alter time
 console.log('Finishing');
 
 // setInterval(buzzer, 2000); //Will not run until the first time period has run and will not be invoked immediately
@@ -18,4 +18,22 @@ function setImmediateInterval(funcToRun, ms) {
   //run a regular interval
   return setInterval(funcToRun, ms);
 }
-setImmediateInterval(buzzer, 2000);
+// setImmediateInterval(buzzer, 2000);
+function destroy() {
+  document.body.innerHTML = `<p>DESTROYED</p>`;
+}
+const bombTimer = setTimeout(destroy, 5000);
+
+window.addEventListener('click', function () {
+  console.log('You clicked! You saved the world');
+  clearTimeout(bombTimer); //stops the timer
+});
+
+const poopInterval = setInterval(function () {
+  console.log('💩');
+  console.log('Hehehe');
+}, 100);
+
+setTimeout(function () {
+  clearInterval(poopInterval);
+}, 500);
