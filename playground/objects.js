b4fd5c1b4c3d2d004387b8473615dcf6d202b7c6
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-03 10:20:11 +1200

    Accessing object values

--- playground/objects.js
+++ playground/objects.js
@@ -7,5 +7,18 @@ const person = {
   'cool-dude': true,
   'really cool': false,
   777: true,
+  clothing: {
+    shirts: 10,
+    pants: 2,
+  },
 };
+person.job = 'Web Developer'; //add a property and value
+person.age = '57'; //alter a property value
+//The values and properties can change but the object itself cannot be reassigned
+
+const personFrozen = Object.freeze(person); //personFrozen cannot be changed (immutable) but original can still  be
+// altered
 console.log(person);
+
+console.log(person.age, person.job); //. notation used to access a value of a property
+console.log(person['age'], person['job']); //[] will access via the property (key) name
