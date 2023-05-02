3d37f16a36668459d5ddba646f87efcafd2284ec
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-02 14:01:04 +1200

    Workingswitch statements

--- playground/switch.js
+++ playground/switch.js
@@ -1,2 +1,27 @@
 const turtle = document.querySelector('.turt');
-console.log(turtle);
+function handleKeyDown(event) {
+  let x = 0;
+  let y = 0;
+  //If it is not an arrow key we don't care
+  if (!event.key.includes('Arrow')) {
+    return;
+  }
+  switch (event.key) {
+    case 'ArrowUp':
+      y = y - 1;
+      break;
+    case 'ArrowZDown':
+      y = y + 1;
+      break;
+    case 'ArrowLeft':
+      x = x - 1;
+      break;
+    case 'ArrowRight':
+      x = x + 1;
+      break;
+    default:
+      console.log('Not a directional key');
+      break;
+  }
+}
+window.addEventListener('keydown', handleKeyDown);
