7c2f4f20c456cec20f76425a23f8ddc52acdb675
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-02 14:24:00 +1200

    Debugged the movement

--- playground/switch.js
+++ playground/switch.js
@@ -1,16 +1,17 @@
 const turtle = document.querySelector('.turt');
+let x = 0;
+let y = 0;
 function handleKeyDown(event) {
-  let x = 0;
-  let y = 0;
   //If it is not an arrow key we don't care
   if (!event.key.includes('Arrow')) {
     return;
   }
+  console.log(event.key);
   switch (event.key) {
     case 'ArrowUp':
       y = y - 1;
       break;
-    case 'ArrowZDown':
+    case 'ArrowDown':
       y = y + 1;
       break;
     case 'ArrowLeft':
@@ -23,5 +24,7 @@ function handleKeyDown(event) {
       console.log('Not a directional key');
       break;
   }
+  turtle.setAttribute('style', `--x: ${x}px; --y: ${y}px`);
+  // turtle.setAttribute('style', `--y: ${y}px`);
 }
 window.addEventListener('keydown', handleKeyDown);
