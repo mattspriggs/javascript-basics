da50cf69492b9a94c3fb94dbd761bdbb300661b7
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-10 13:35:52 +1200

    Working on painting the context to pixelate the face

--- exercises/face-detection/dist/face.78b1ac0f.js
+++ exercises/face-detection/dist/face.78b1ac0f.js
@@ -4,13 +4,12 @@
 const video = document.querySelector(".webcam");
 const canvas = document.querySelector(".video");
 const ctx = canvas.getContext("2d");
-ctx.strokeStyle = "#ffc600";
-ctx.lineWidth = 2;
 const faceCanvas = document.querySelector(".face");
-const faceCtx = canvas.getContext("2d");
+const faceCtx = faceCanvas.getContext("2d");
 const faceDetector = new window.FaceDetector({
     fastMode: true
 });
+const SIZE = 10;
 //Write a function that will populate the users video
 async function populateVideo() {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -30,14 +29,26 @@ async function populateVideo() {
 }
 async function detect() {
     const faces = await faceDetector.detect(video);
-    console.log(faces.length);
     //ask the browser when the next animation frame is and tell it to run detect for us
     faces.forEach(drawFace);
+    faces.forEach(censor);
     requestAnimationFrame(detect); //recursion function - function calls itself
 }
 function drawFace(face) {
     const { width , height , top , left  } = face.boundingBox;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "#ffc600";
+    ctx.lineWidth = 2;
     ctx.strokeRect(left, top, width, height);
+}
+function censor({ boundingBox: face  }) {
+    //Draw teh small face
+    faceCtx.drawImage(//5 source arguments
+    video, face.x, face.y, face.width, face.height, //4 draw arguments
+    face.x, face.y, SIZE, SIZE);
+    //Take the small face and draw in large space
+    faceCtx.drawImage(faceCanvas, face.x, face.y, SIZE, SIZE, //Drawing arguments
+    face.x, face.y, face.width, face.height);
 }
 populateVideo().then(detect);
 
