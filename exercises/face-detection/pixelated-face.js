ed026e53cd46917e5cf395cd3f4f100b277623e8
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-10 12:25:09 +1200

    Working to get face tection working

--- exercises/face-detection/pixelated-face.js
+++ exercises/face-detection/pixelated-face.js
@@ -1,4 +1,36 @@
 // The face detection does not work on all browsers and operating systems.
 // If you are getting a `Face detection service unavailable` error or similar,
 // it's possible that it won't work for you at the moment.
-console.log('it works');
+
+const video = document.querySelector('.webcam');
+
+const canvas = document.querySelector('.video');
+const ctx = canvas.getContext('2d');
+
+const faceCanvas = document.querySelector('.face');
+const faceCtx = canvas.getContext('2d');
+
+const faceDetector = new window.FaceDetector({ fastMode: true });
+
+//Write a function that will populate the users video
+async function populateVideo() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 1280, height: 720 },
+  });
+  video.srcObject = stream;
+  await video.play();
+  //size the canvases to be the same size as the video
+  console.log(video.videoWidth, video.videoHeight);
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
+}
+
+async function detect() {
+  const faces = await faceDetector.detect(video);
+  console.log(faces.length);
+  //ask the browser when the next animation frame is and tell it to run detect for us
+  requestAnimationFrame(detect); //recursion function - function calls itself
+}
+populateVideo().then(detect);
