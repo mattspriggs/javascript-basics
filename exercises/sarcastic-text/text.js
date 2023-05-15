110db4a62188352563d58dfa8d7756f29d22205f
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-16 11:41:23 +1200

    Working with text area captures

--- exercises/sarcastic-text/text.js
+++ exercises/sarcastic-text/text.js
@@ -1,1 +1,12 @@
-console.log('it works');
+const textarea = document.querySelector('[name="text"]');
+const result = document.querySelector('.result');
+const filterInputs = document.querySelectorAll('[name="filter"]');
+
+// console.log(textArea);
+// console.log(result);
+// console.log(filterInputs);
+function transformText(text) {
+  console.log(text);
+  result.textContent = text;
+}
+textarea.addEventListener('input', (e) => transformText(e.target.value));
