b40ed3870185dc39d2286fb9d9d00aea551ce76b
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-16 11:55:53 +1200

    Working onf the sarcastic filter

--- exercises/sarcastic-text/text.js
+++ exercises/sarcastic-text/text.js
@@ -6,7 +6,21 @@ const filterInputs = document.querySelectorAll('[name="filter"]');
 // console.log(result);
 // console.log(filterInputs);
 function transformText(text) {
-  console.log(text);
+  //take the text and loop over each letter
+  const mod = Array.from(text).map(filters.sarcastic);
+  console.log(mod);
   result.textContent = text;
 }
+const filters = {
+  sarcastic: function (letter, index) {
+    //if it is odd, it will return 1 and that is truthy so this statement will trip
+    if (index % 2) {
+      return letter.toUpperCase();
+    }
+    // if it is even it will return zero and it will lowercase it
+    return letter.toLowerCase();
+  },
+  funky: function () {},
+  unable: function () {},
+};
 textarea.addEventListener('input', (e) => transformText(e.target.value));
