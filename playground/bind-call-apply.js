d411fc916200c9a1fe3175c22c2552af1a30a44e
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-26 16:25:20 +1200

    More work with bind and objects

--- playground/bind-call-apply.js
+++ playground/bind-call-apply.js
@@ -20,5 +20,25 @@ const sayHi3 = person.sayHi.bind(jenna);
 console.log(sayHi3());
 
 //Query Selector example
-const $ = document.querySelector;
-console.log($('p')); //Illegal invocation error
+//By calling bind against query selector sets it to use document as the this value when querySelector is run
+const $ = document.querySelector.bind(document);
+const lookFor = document.querySelectorAll.bind(document);
+console.log($('p'));
+const wrapper = document.querySelector('.wrapper');
+const p = wrapper.querySelector('p');
+console.log(p);
+console.log(lookFor('p'));
+
+const bill = {
+  total: 1000,
+  calculate: function (taxRate) {
+    return this.total + this.total * taxRate;
+  },
+};
+const total = bill.calculate(0.15);
+console.log(total);
+
+const calc = bill.calculate.bind(bill);
+const calc2 = bill.calculate.bind({ total: 500 });
+console.log(calc(0.15));
+console.log(calc2(0.15));
