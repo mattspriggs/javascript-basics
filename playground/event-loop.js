eb182ae815a9d51d4bb6bf9923b4f9512271b34c
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-29 14:28:24 +1200

    Working with the call stack and the web API

--- playground/event-loop.js
+++ playground/event-loop.js
@@ -1,1 +1,15 @@
-console.log('it works');
+//Single threaded language so only one thing can happen at a time
+// console.log('Starting');
+// setTimeout(function () {
+//   console.log('Running...');
+// }, 0); //Will run the callback in 2 seconds - output is Starting/Ending/Running due to async
+// console.log('Ending');
+
+const go = document.querySelector('.go');
+//Change the text to GO when clicked
+//Make it a circle after 2 seconds
+//Make it red after .5 seconds
+//make it square after .25 seconds
+//make it purple after .3 seconds
+//fade out after .5 seconds
+//Finish
