cbb944f00870fd8f3be44035fd86f17d53b8c884
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-29 15:31:41 +1200

    Working with the use of promises

--- playground/promises.js
+++ playground/promises.js
@@ -1,11 +1,29 @@
-async function makePizza() {
-  const pizzaPromise = new Promise(function (resolve, reject) {
-    //When you are ready you can resolve the promise
-    resolve('🍕');
+function makePizza(toppings) {
+  return new Promise(function (resolve, reject) {
+    //Wait 1 second for the pizza to cook
+    setTimeout(function () {
+      //When you are ready you can resolve the promise
+      resolve(
+        `Here is your pizza 🍕with the toppings of: ${toppings.join(' ')}`
+      );
+    }, 1000);
     //If something went wrong you can reject the promise
   });
-  return pizzaPromise;
 }
 
-const pizza = makePizza();
-console.log(pizza);
+const pepperoniPromise = makePizza(['pepperoni']);
+const canadianPromise = makePizza(['pepperoni', 'mushrooms', 'onions']);
+console.log(pepperoniPromise);
+console.log(canadianPromise);
+console.log('Starting');
+// pepperoniPromise.then(function (pizza) {
+//   //To access the info in the promise that is returned
+//   console.log('Ahh got it!');
+//   console.log(pizza);
+// });
+// console.log('Finishing');
+// canadianPromise.then(function (pizza) {
+//   //To access the info in the promise that is returned
+//   console.log('Ahh got it!');
+//   console.log(pizza);
+// });
