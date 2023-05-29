0e798db02861d437f5cfb51e14173804ddca66da
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-29 15:55:04 +1200

    Chaining with .then in promises

--- playground/promises.js
+++ playground/promises.js
@@ -1,21 +1,22 @@
-function makePizza(toppings) {
+function makePizza(toppings = []) {
   return new Promise(function (resolve, reject) {
+    const amountOfTimeToBake = 500 + toppings.length * 250;
     //Wait 1 second for the pizza to cook
     setTimeout(function () {
       //When you are ready you can resolve the promise
       resolve(
         `Here is your pizza 🍕with the toppings of: ${toppings.join(' ')}`
       );
-    }, 1000);
+    }, amountOfTimeToBake);
     //If something went wrong you can reject the promise
   });
 }
 
-const pepperoniPromise = makePizza(['pepperoni']);
-const canadianPromise = makePizza(['pepperoni', 'mushrooms', 'onions']);
-console.log(pepperoniPromise);
-console.log(canadianPromise);
-console.log('Starting');
+// const pepperoniPromise = makePizza(['pepperoni']);
+// const canadianPromise = makePizza(['pepperoni', 'mushrooms', 'onions']);
+// console.log(pepperoniPromise);
+// console.log(canadianPromise);
+// console.log('Starting');
 // pepperoniPromise.then(function (pizza) {
 //   //To access the info in the promise that is returned
 //   console.log('Ahh got it!');
@@ -27,3 +28,31 @@ console.log('Starting');
 //   console.log('Ahh got it!');
 //   console.log(pizza);
 // });
+makePizza(['pepperoni'])
+  .then(function (pizza) {
+    console.log(pizza);
+    return makePizza(['ham', 'cheese']);
+  })
+  .then(function (pizza) {
+    console.log(pizza);
+    return makePizza(['hot pepper', 'onion', 'feta']);
+  })
+  .then(function (pizza) {
+    console.log(pizza);
+    return makePizza();
+  })
+  .then(function (pizza) {
+    console.log(pizza);
+    return makePizza([
+      'hot pepper',
+      'onion',
+      'feta',
+      'ham',
+      'cheese',
+      'pepperoni',
+    ]);
+  })
+  .then((pizza) => {
+    console.log('All done! Here is your last pizza -');
+    console.log(pizza);
+  });
