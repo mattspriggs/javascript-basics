8d7e5499495a1f8212f77656b05302a20e0d112e
Matt Spriggs <mspriggs1@cnm.edu>
2023-05-30 15:04:45 +1200

    Working with async-await

--- playground/async-await.js
+++ playground/async-await.js
@@ -1,1 +1,27 @@
-console.log('it works');
+function wait(ms = 0) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function go() {
+  console.log('starting');
+  await wait(2000);
+  console.log('ending');
+}
+go();
+
+//function declaration
+async function fd() {}
+//Arrow functions
+const arrowFn = async () => {};
+//callback function
+window.addEventListener('click', async function () {});
+
+//object methods
+const person = {
+  // method
+  sayHi: async function () {},
+  // method shorthand
+  async sayHello() {},
+  //function property
+  sayHey: async () => {},
+};
