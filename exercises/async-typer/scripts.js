90cf506d35fb9b2bd8e512b845fed7c4e5010586
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-14 10:48:24 +1200

    Working with the random number generation

---
+++ exercises/async-typer/scripts.js
@@ -0,0 +1,11 @@
+function wait(ms = 0) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
+  return Math.floor(randomNumber * (max - min) + min);
+}
+
+function draw(element) {
+  console.log(element);
+}
