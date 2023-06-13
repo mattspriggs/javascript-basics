b7339c45424c009f6c87c78a0f50950257a613b0
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-14 10:56:19 +1200

    Isolating letters to start to mimic typing

--- exercises/async-typer/scripts.js
+++ exercises/async-typer/scripts.js
@@ -6,6 +6,15 @@ function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
 
+//async for of loop
 function draw(element) {
-  console.log(element);
+  const text = element.textContent;
+  let soFar = '';
+  for (const letter of text) {
+    console.log(letter);
+    soFar += letter;
+    console.log(soFar);
+  }
 }
+
+const els = document.querySelectorAll('[data-type]').forEach(draw);
