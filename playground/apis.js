1e136022a605861eedc425fe32c5eda38dca00ae
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-14 11:51:46 +1200

    Working with APIs

--- playground/apis.js
+++ playground/apis.js
@@ -1,1 +1,14 @@
-console.log('it works');
+//JavaScript Object Notation -JSON
+//A - Asynchronous
+//J - JavaScript
+//A - And
+//X - XML
+//AJAX - fetching data from an API and displaying it on the page
+const endpoint = `https://api.github.com/users/wesbos`;
+function handleError(err) {
+  console.log('OH NO!!!!');
+  console.log(err);
+}
+const wesPromise = fetch(endpoint);
+wesPromise.then((data) => console.log(data)).catch(handleError);
+//`https://recipes.beginnerjavascript.com/api`;
