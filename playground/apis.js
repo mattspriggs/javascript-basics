de62001d6d519a7e9bd2fdceff8c2f3f56de678f
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-14 12:21:12 +1200

    Displaying the JSON on the page

--- playground/apis.js
+++ playground/apis.js
@@ -4,11 +4,33 @@
 //A - And
 //X - XML
 //AJAX - fetching data from an API and displaying it on the page
-const endpoint = `https://api.github.com/users/wesbos`;
+const baseEndpoint = `https://api.github.com`;
+const usersEndpoint = `${baseEndpoint}/users`;
+const userEl = document.querySelector('.user');
+
+async function displayUser(username) {
+  userEl.textContent = 'loading ...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();
+  console.log(data);
+  userEl.textContent = `${data.name} - ${data.blog}`;
+}
 function handleError(err) {
   console.log('OH NO!!!!');
   console.log(err);
+  userEl.textContent = `Something went wrong!: ${err}`;
 }
-const wesPromise = fetch(endpoint);
-wesPromise.then((data) => console.log(data)).catch(handleError);
+displayUser('mattspriggs').catch(handleError);
+
+// userEl.textContent = 'loading ...';
+// const wesPromise = fetch(endpoint);
+// wesPromise
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     userEl.textContent = `${data.name} - ${data.blog}`;
+//   })
+//   .catch(handleError);
 //`https://recipes.beginnerjavascript.com/api`;
