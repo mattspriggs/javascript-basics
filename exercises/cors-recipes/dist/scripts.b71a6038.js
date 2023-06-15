f372956ce76bbbbce8b5b584731c6408e1df8355
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-15 15:45:59 +1200

    Getting user input in the query field

--- exercises/cors-recipes/dist/scripts.b71a6038.js
+++ exercises/cors-recipes/dist/scripts.b71a6038.js
@@ -130,10 +130,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 //& - joiner for additional options or parameters
 //p=3
 const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
+// const proxy = `https://cors-anywhere.herokuapp.com/`;${proxy}${baseEndpoint}?q=${query}
+const form = document.querySelector('form.search');
 async function fetchRecipes(query) {
   const res = await fetch(`${baseEndpoint}?q=${query}`);
   const data = await res.json();
+  console.log(data);
 }
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(event.currentTarget.query.value);
+}
+form.addEventListener('submit', handleSubmit);
 fetchRecipes('pizza');
 //C - Cross - CORS policy has to allow you to transfer information and has to be on the server providing the information
 //O - Origin - by default you can not share information between websites
