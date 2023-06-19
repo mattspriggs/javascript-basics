b8181ef406e6e8a7aff7be40240493f68add83af
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-20 11:52:22 +1200

    Adding the loader function to the button

--- exercises/dad-jokes/jokes.js
+++ exercises/dad-jokes/jokes.js
@@ -1,5 +1,7 @@
 const jokeButton = document.querySelector('.getJoke');
+const jokeSpan = jokeButton.querySelector('.jokeText');
 const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
 
 const buttonText = [
   'Ugh.',
@@ -13,10 +15,14 @@ const buttonText = [
 ];
 
 async function fetchJoke() {
+  //turn loader on
+  loader.classList.remove('hidden');
   const response = await fetch('https://icanhazdadjoke.com', {
     headers: { Accept: 'application/json' },
   });
   const data = await response.json();
+  //turn the loader off
+  loader.classList.add('hidden');
   return data;
 }
 
