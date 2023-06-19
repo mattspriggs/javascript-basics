eb833f8fd762f2a21c846012c046bbae18e152f8
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-20 11:06:57 +1200

    Fetching from ICanHaz

--- exercises/dad-jokes/jokes.js
+++ exercises/dad-jokes/jokes.js
@@ -1,1 +1,23 @@
-console.log('it works');
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
+const buttonText = [
+  'Ugh.',
+  '🤦🏻‍♂️',
+  'omg dad.',
+  'you are the worst',
+  'seriously',
+  'stop it.',
+  'please stop',
+  'that was the worst one',
+];
+
+async function fetchJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: { Accept: 'application/json' },
+  });
+  const joke = await response.json();
+  console.log(joke);
+}
+
+fetchJoke();
