f392d86f6798702bd9f125d14f99615bbab6aee7
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-22 13:12:59 +1200

    Debugged the module work

--- playground/modules/scripts.js
+++
@@ -1,5 +0,0 @@
-import { returnHi } from './index.html';
-
-console.log('it works');
-console.log(returnHi(matt));
-const name = 'matt';
---
+++ playground/modules/scripts/scripts.js
@@ -0,0 +1,5 @@
+import { returnHi } from '/playground/modules/utils/utils.js';
+
+console.log('it works');
+console.log(returnHi('matt'));
+const name = 'matt';
