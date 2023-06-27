f1e8a6c13fae50fe83b0a49fbcef5f2457a68eae
Matt Spriggs <mspriggs1@cnm.edu>
2023-06-27 14:28:31 +1200

    Moved currencies to a separate module

--- exercises/currency-converter-refactor/money.js
+++ exercises/currency-converter-refactor/money.js
@@ -1,3 +1,5 @@
+import { currencies } from './src/currencies.js';
+
 const fromSelect = document.querySelector('[name="from_currency"]');
 const fromInput = document.querySelector('[name="from_amount"]');
 const toSelect = document.querySelector('[name="to_currency"]');
@@ -5,41 +7,6 @@ const toEl = document.querySelector('.to_amount');
 const endpoint = 'https://api.apilayer.com/exchangerates_data/latest';
 const ratesByBase = {};
 const form = document.querySelector('.app form');
-
-const currencies = {
-  USD: 'United States Dollar',
-  AUD: 'Australian Dollar',
-  BGN: 'Bulgarian Lev',
-  BRL: 'Brazilian Real',
-  CAD: 'Canadian Dollar',
-  CHF: 'Swiss Franc',
-  CNY: 'Chinese Yuan',
-  CZK: 'Czech Republic Koruna',
-  DKK: 'Danish Krone',
-  GBP: 'British Pound Sterling',
-  HKD: 'Hong Kong Dollar',
-  HRK: 'Croatian Kuna',
-  HUF: 'Hungarian Forint',
-  IDR: 'Indonesian Rupiah',
-  ILS: 'Israeli New Sheqel',
-  INR: 'Indian Rupee',
-  JPY: 'Japanese Yen',
-  KRW: 'South Korean Won',
-  MXN: 'Mexican Peso',
-  MYR: 'Malaysian Ringgit',
-  NOK: 'Norwegian Krone',
-  NZD: 'New Zealand Dollar',
-  PHP: 'Philippine Peso',
-  PLN: 'Polish Zloty',
-  RON: 'Romanian Leu',
-  RUB: 'Russian Ruble',
-  SEK: 'Swedish Krona',
-  SGD: 'Singapore Dollar',
-  THB: 'Thai Baht',
-  TRY: 'Turkish Lira',
-  ZAR: 'South African Rand',
-  EUR: 'Euro',
-};
 
 // const myHeaders = new Headers();
 // myHeaders.append('apikey', API_KEY);
